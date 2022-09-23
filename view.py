244396a3e20b99a7412fa7166ddd8e536dc1988b
James Rivett <jrivett@students.kennesaw.edu>
2022-09-22 23:37:40 -0400

    1.0

--- view.py
+++ view.py
@@ -10,6 +10,7 @@ class View(tk.Frame):
         self.PRICE_mediumpizza = tk.DoubleVar(value=12.99)
         self.PRICE_smallpizza = tk.DoubleVar(value=10.99)
 
+        #################### MENU ITEMS ####################
         LABEL_largepizza = ttk.Label(self, text='Large Pizza | ' + str(self.PRICE_largepizza.get()))
         LABEL_largepizza.grid(row=0, column=0, sticky=tk.W)
         BUTTON_addToCart_largepizza = ttk.Button(self, text="Add to Cart", command=lambda: self.addToCart(self.PRICE_largepizza.get()))
@@ -19,15 +20,19 @@ class View(tk.Frame):
         LABEL_mediumpizza.grid(row=1, column=0, sticky=tk.W)
         BUTTON_addToCart_mediumpizza = ttk.Button(self, text="Add to Cart", command=lambda: self.addToCart(self.PRICE_mediumpizza.get()))
         BUTTON_addToCart_mediumpizza.grid(row=1, column=1, sticky=tk.E) 
-
+        
         LABEL_smallpizza = ttk.Label(self, text='Small Pizza | ' + str(self.PRICE_smallpizza.get()))
         LABEL_smallpizza.grid(row=2, column=0, sticky=tk.W)
         BUTTON_addToCart_smallpizza = ttk.Button(self, text="Add to Cart", command=lambda: self.addToCart(self.PRICE_smallpizza.get()))
         BUTTON_addToCart_smallpizza.grid(row=2, column=1, sticky=tk.E) 
         
+        #################### CART ####################
+        LABEL_cart = ttk.Label(self, text='Total:')
+        LABEL_cart.grid(row=3, column=0, sticky=(tk.S + tk.W), pady=10)
+
         self.total = tk.DoubleVar(value=0.00)
         LABEL_total = ttk.Label(self, textvariable=self.total)
-        LABEL_total.grid(row=3, column=2, sticky=tk.S)
+        LABEL_total.grid(row=4, column=0, sticky=tk.S)
 
     def addToCart(self, price):
         val = self.total.get()
